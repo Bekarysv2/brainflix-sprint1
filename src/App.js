c3f3ddd24c8233ea1d0e3e491d5920511c7f163b
Bekarys Alimov <beka.alimov2000@gmail.com>
2024-02-25 19:53:21 +0000

    Added VideoDetails js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import {useState} from 'react';
 import './App.css';
 import NavigationBar from './components/Navigation-bar/Nav-bar';
 import MainVideo from './components/MainVideo/Main-video';
+import VideoDetails from './components/VideoDetails/VideoDetails';
+
+import VideoList from './data/video-details.json';
 
 function App() {
+  const [videos, setVideos] = useState(VideoList);
+  const [selectedVideo, setSelectedVideo] = useState(VideoList[0]);
+
+  const clickHandler = (video) => {
+    setSelectedVideo(video);
+  }
+
   return (
     <>
-    <NavigationBar />
-    <MainVideo />
+      <NavigationBar />
+      <MainVideo video={selectedVideo} />
+      <VideoDetails video={selectedVideo} />
     </>
   );
 }
+
 
 export default App;

ff9f7cb221d0cec069385184d42458307a577359
Bekarys Alimov <beka.alimov2000@gmail.com>
2024-02-26 21:47:25 +0000

    Added scss styles

--- src/components/Add-Comment/New-comment.js
+++ src/components/Add-Comment/New-comment.js
@@ -1,11 +1,14 @@
 import './New-comment.scss';
+import Mohan from '../../assets/Images/Mohan-muruge.jpg';
 
 const NewComment = ({commentsCount}) => {
     return (
         <section className="submit-comment">
             <p className="comment-count">{commentsCount} Comments</p>
             <div className="submit-comment__container">
-                <div className="submit-comment__container__profile"></div>
+                <div className="submit-comment__container__profile">
+                    <img src={Mohan}></img>
+                </div>
                 <form className="submit-comment__container__form">
                     <label for="comment">JOIN THE CONVERSATION</label>
                     <div className="submit-comment__container__form__fields">
--- src/components/Navigation-bar/Nav-bar.js
+++ src/components/Navigation-bar/Nav-bar.js
@@ -8,27 +8,24 @@ const NavigationBar = () => {
 
   return (
     <nav className="nav">
-      <a src="/">
         <img src={logo} alt="BrainFlix Logo" className="nav__logo" />
-      </a>
       <div className="nav__container">
-        <input
-          type="search"
-          placeholder="      Search"
-          className="nav__search"
-          id="search-input"
-        ></input>
-        <img src={searchIcon} alt="search-icon" className="nav__container__search"></img>
-        <a src="/" className="nav__upload-buttons button">
-          UPLOAD
-        </a>
-        <div className="nav__profle">
+          <input
+            type="search"
+            placeholder="Search"
+            className="nav__search"
+            id="search-input"
+          ></input>
+        <button type="submit" className="nav__upload-button">
+        UPLOAD
+        </button>
+        <div className="nav__container__profile">
           <img src={mohanImg} alt="Mohan-profile" className="nav__mohan"></img>
         </div>
       </div>
-      <a src="/" className="nav__upload-buttons button-mobile">
+      <button type="submit" className="nav__upload-button-mobile">
         UPLOAD
-      </a>
+      </button>
     </nav>
   );
 }
